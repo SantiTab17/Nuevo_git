bb259e204140665e300e94d013c26be248d6fde5
SantiTab17 <145880351+SantiTab17@users.noreply.github.com>
2023-10-09 23:16:23 -0500

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,21 @@
+let contador = 0;
+
+function FizzBuzz(n) {
+    while(n < 100) {
+        n++;
+        if(n % 3 === 0){
+            console.log('Fizz');
+        }
+        else if(n % 5 ===0) {
+            console.log('Buzz');
+        }
+        else if(n % 3 === 0 && n % 5 === 0) {
+            console.log('FizzBuzz');
+        }
+        else {
+            console.log(n);
+        }
+    }
+}
+
+FizzBuzz(contador);
